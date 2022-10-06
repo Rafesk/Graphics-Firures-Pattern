a4de0123cd835f65b8d679092acd12f46ffae009
Ksenia Balalaeva <balalaeva.ks@mail.ru>
2022-10-06 18:40:15 +0300

    AddManipulat

---
+++ Фигуры 2/Manipulator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Фигуры_2
+{
+    internal class Manipulator :Figure
+    {
+        Figure fig;
+        int activePoint = 0;
+        public override void Draw(Graphics g)
+        {
+            
+        }
+
+        public override bool IsIn(int x, int y)
+        {
+            return false;
+        }
+        public void Location(int x, int y)
+        {
+
+        }
+        public void Resize(int x, int y)
+        {
+
+        }
+        public void SetFigure(Figure f)
+        {
+
+        }
+        public void Drag(int dx, int dy)
+        {
+
+        }
+    }
+}
